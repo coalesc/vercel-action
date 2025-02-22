b0c0cbf3e8fa2a2dce93abb53445453d57d5643c
Samuel Corsi-House <sam@chouse.dev>
2025-02-21 22:38:48 -0500

    debug

--- dist/index.js
+++ dist/index.js
@@ -31973,7 +31973,7 @@ async function run() {
     }
     core.startGroup("Setting deployment status");
     const { status, data } = await rest.createDeployment(ref);
-    console.log(data);
+    console.log(status);
     if (status !== 201) {
         core.warning("Couldn't create deployment");
     }
